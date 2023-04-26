aba83db550541adabe2ace850d704f785e28ffb4
agung <agung.kurniawan@oddy.id>
2023-04-26 16:41:35 +0700

    [865c3v4qh] set label optional, reorder params, update typography

--- customer-component/src/main/java/com/planetoto/customer_component/foundation/PlanetTypography.kt
+++ customer-component/src/main/java/com/planetoto/customer_component/foundation/PlanetTypography.kt
@@ -16,22 +16,55 @@ sealed class PlanetTypography(
     val lineHeight: TextUnit?,
     val weight: FontWeight?
 ) {
-    object Headline : PlanetTypography(size = 32.sp, lineHeight = 38.5.sp, weight = FontWeight.W700)
-    object TitleScreen : PlanetTypography(size = 26.sp, lineHeight = 32.sp, weight = FontWeight.W700)
-    object TitleSection : PlanetTypography(size = 22.sp, lineHeight = 28.sp, weight = FontWeight.W700)
-    object TitleSubsection : PlanetTypography(size = 18.sp, lineHeight = 24.sp, weight = FontWeight.W600)
-    object TitleBody : PlanetTypography(size = 18.sp, lineHeight = 24.sp, weight = FontWeight.W700)
-    object SubtitleBody : PlanetTypography(size = 14.sp, lineHeight = 22.sp, weight = FontWeight.W600)
-    object CardTitle : PlanetTypography(size = 14.sp, lineHeight = 22.sp, weight = FontWeight.W700)
-    object BodyLarge : PlanetTypography(size = 16.sp, lineHeight = 27.sp, weight = FontWeight.W400)
-    object BodyLargeBold : PlanetTypography(size = 16.sp, lineHeight = 27.sp, weight = FontWeight.W700)
-    object BodyDefault14 : PlanetTypography(size = 14.sp, lineHeight = 24.sp, weight = FontWeight.W400)
-    object BodyDefaultBold : PlanetTypography(size = 14.sp, lineHeight = 24.sp, weight = FontWeight.W700)
-    object BodyDefault12 : PlanetTypography(size = 12.sp, lineHeight = 22.sp, weight = FontWeight.W400)
-    object CaptionHelper : PlanetTypography(size = 11.sp, lineHeight = 20.sp, weight = FontWeight.W400)
-    object CaptionUppercase : PlanetTypography(size = 11.sp, lineHeight = null, weight = FontWeight.W600)
-    object CaptionLabelOrTag : PlanetTypography(size = 11.sp, lineHeight = null, weight = FontWeight.W600)
-    object CaptionCodeType : PlanetTypography(size = 14.sp, lineHeight = 24.sp, weight = FontWeight.W400)
-    object LabelMediumButton : PlanetTypography(size = 14.sp, lineHeight = null, weight = FontWeight.W600)
-    object LabelSmallButton : PlanetTypography(size = 12.sp, lineHeight = null, weight = FontWeight.W700)
+    object Headline : PlanetTypography(size = 32.sp, lineHeight = 38.5.sp, weight = FontWeight.Bold)
+
+    object TitleScreen :
+        PlanetTypography(size = 26.sp, lineHeight = 32.sp, weight = FontWeight.Bold)
+
+    object TitleSection :
+        PlanetTypography(size = 22.sp, lineHeight = 28.sp, weight = FontWeight.Bold)
+
+    object TitleSubsection :
+        PlanetTypography(size = 18.sp, lineHeight = 24.sp, weight = FontWeight.SemiBold)
+
+    object TitleBody : PlanetTypography(size = 18.sp, lineHeight = 24.sp, weight = FontWeight.Bold)
+    object SubtitleBody :
+        PlanetTypography(size = 14.sp, lineHeight = 22.sp, weight = FontWeight.SemiBold)
+
+    object CardTitle : PlanetTypography(size = 14.sp, lineHeight = 22.sp, weight = FontWeight.Bold)
+    object BodyLarge :
+        PlanetTypography(size = 16.sp, lineHeight = 27.sp, weight = FontWeight.Normal)
+
+    object BodyLargeBold :
+        PlanetTypography(size = 16.sp, lineHeight = 27.sp, weight = FontWeight.Bold)
+
+    object BodyDefault14 :
+        PlanetTypography(size = 14.sp, lineHeight = 24.sp, weight = FontWeight.Normal)
+
+    object BodyDefaultBold :
+        PlanetTypography(size = 14.sp, lineHeight = 24.sp, weight = FontWeight.Bold)
+
+    object SmallBody :
+        PlanetTypography(size = 12.sp, lineHeight = 22.sp, weight = FontWeight.Normal)
+
+    object CaptionHelper :
+        PlanetTypography(size = 11.sp, lineHeight = 20.sp, weight = FontWeight.Normal)
+
+    object CaptionUppercase :
+        PlanetTypography(size = 11.sp, lineHeight = null, weight = FontWeight.SemiBold)
+
+    object CaptionLabelOrTag :
+        PlanetTypography(size = 11.sp, lineHeight = null, weight = FontWeight.SemiBold)
+
+    object CaptionSmallLabel :
+        PlanetTypography(size = 9.sp, lineHeight = null, weight = FontWeight.SemiBold)
+
+    object CaptionCodeType :
+        PlanetTypography(size = 14.sp, lineHeight = 24.sp, weight = FontWeight.Normal)
+
+    object LabelBigButton :
+        PlanetTypography(size = 14.sp, lineHeight = null, weight = FontWeight.SemiBold)
+
+    object LabelMediumButton :
+        PlanetTypography(size = 12.sp, lineHeight = null, weight = FontWeight.Bold)
 }
