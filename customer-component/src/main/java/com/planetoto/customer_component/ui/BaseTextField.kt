b4c32afda67cbb930f4ee63d42b936a700820ce4
agung <agung.kurniawan@oddy.id>
2023-05-31 06:37:21 +0700

    [] set not clickable when disabled

--- customer-component/src/main/java/com/planetoto/customer_component/ui/BaseTextField.kt
+++ customer-component/src/main/java/com/planetoto/customer_component/ui/BaseTextField.kt
@@ -1,16 +1,31 @@
 package com.planetoto.customer_component.ui
 
-import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -105,9 +120,12 @@ internal fun BaseTextField(
                             width = 1.5.dp,
                             color = borderColor.color,
                             shape = RoundedCornerShape(8.dp)
-                        ).run {
-                            if (onClick != null) clickable(onClick = onClick) else this
-                        },
+                        )
+                        .then(
+                            if (isFocused && onClick != null) {
+                                Modifier.clickable(onClick = onClick)
+                            } else Modifier
+                        ),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     prefixBox?.invoke(height)
