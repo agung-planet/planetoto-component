ab3ae28c7e5105b67d977d3bc8f0f8d075c376cb
kristo <kristhokhosim@gmail.com>
2023-06-07 01:06:49 +0700

    [] change container to constraint layout

--- customer-component/src/main/java/com/planetoto/customer_component/ui/PlanetImageBadge.kt
+++ customer-component/src/main/java/com/planetoto/customer_component/ui/PlanetImageBadge.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
@@ -13,6 +13,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.planetoto.customer_component.foundation.PlanetColors
 import com.planetoto.customer_component.utils.disableRipple
 
@@ -29,11 +30,11 @@ fun PlanetImageBadge(
     isRounded: Boolean = false,
     onClick: () -> Unit
 ) {
-    Box(modifier = modifier.disableRipple(onClick = onClick)) {
+    ConstraintLayout(modifier = modifier.disableRipple(onClick = onClick)) {
+        val (icon, badge) = createRefs()
 
         val paddingTop = if (count > 0) 8.dp else 0.dp
         val imageModifier = if (isRounded) Modifier
-            .padding(top = paddingTop)
             .clip(shape = CircleShape)
             .border(
                 width = 1.dp,
@@ -42,7 +43,12 @@ fun PlanetImageBadge(
             ) else Modifier.padding(vertical = 8.dp, horizontal = 10.dp)
 
         Image(
-            modifier = imageModifier.size(width = imageWidth, height = imageHeight),
+            modifier = imageModifier
+                .constrainAs(icon) {
+                    top.linkTo(parent.top, paddingTop)
+                    start.linkTo(parent.start)
+                }
+                .size(width = imageWidth, height = imageHeight),
             painter = painter,
             contentDescription = "ic_image"
         )
@@ -50,8 +56,64 @@ fun PlanetImageBadge(
         if (count > 0) {
             PlanetBadgeCount(
                 modifier = Modifier
-                    .padding(start = 20.dp)
-                    .align(Alignment.TopStart),
+                    .constrainAs(badge) {
+                        top.linkTo(parent.top)
+                        end.linkTo(icon.end, (-10).dp)
+                    },
+                badgeCorner = badgeCorner,
+                badgeBorder = badgeBorder,
+                badgeBorderColor = badgeBorderColor,
+                count = count
+            )
+        }
+    }
+}
+
+@Composable
+fun PlanetImageBadge(
+    modifier: Modifier = Modifier,
+    iconWidth: Dp = 20.dp,
+    iconHeight: Dp = 24.dp,
+    badgeCorner: Dp = 20.dp,
+    badgeBorder: Dp = 1.5.dp,
+    badgeBorderColor: PlanetColors.Solid = PlanetColors.Solid.neutralWhite,
+    iconPainter: Painter,
+    iconTint: PlanetColors.Solid,
+    count: Int = 0,
+    isRounded: Boolean = false,
+    onClick: () -> Unit
+) {
+    ConstraintLayout(modifier = modifier.disableRipple(onClick = onClick)) {
+        val (icon, badge) = createRefs()
+
+        val paddingTop = if (count > 0) 8.dp else 0.dp
+        val imageModifier = if (isRounded) Modifier
+            .clip(shape = CircleShape)
+            .border(
+                width = 1.dp,
+                color = PlanetColors.Solid.neutralBorder01.color,
+                shape = CircleShape
+            ) else Modifier.padding(vertical = 8.dp, horizontal = 10.dp)
+
+        Icon(
+            modifier = imageModifier
+                .constrainAs(icon) {
+                    top.linkTo(parent.top, paddingTop)
+                    start.linkTo(parent.start)
+                }
+                .size(width = iconWidth, height = iconHeight),
+            painter = iconPainter,
+            tint = iconTint.color,
+            contentDescription = "ic_image"
+        )
+
+        if (count > 0) {
+            PlanetBadgeCount(
+                modifier = Modifier
+                    .constrainAs(badge) {
+                        top.linkTo(parent.top)
+                        end.linkTo(icon.end, (-10).dp)
+                    },
                 badgeCorner = badgeCorner,
                 badgeBorder = badgeBorder,
                 badgeBorderColor = badgeBorderColor,
@@ -64,15 +126,29 @@ fun PlanetImageBadge(
 @Preview
 @Composable
 private fun PlanetImageBadgePreview() {
-    Box {
-        PlanetImageBadge(
-            count = 80,
-            isRounded = true,
-            imageHeight = 32.dp,
-            imageWidth = 32.dp,
-            painter = painterResource(id = com.planetoto.customer_component.R.drawable.placeholder_avatar)
-        ) {
+    Column() {
+        Box {
+            PlanetImageBadge(
+                count = 80,
+                isRounded = true,
+                iconHeight = 40.dp,
+                iconWidth = 40.dp,
+                iconTint = PlanetColors.Solid.blue07,
+                iconPainter = painterResource(id = com.planetoto.customer_component.R.drawable.placeholder_avatar)
+            ) {
 
+            }
+        }
+        Box {
+            PlanetImageBadge(
+                count = 80,
+                isRounded = true,
+                imageHeight = 32.dp,
+                imageWidth = 32.dp,
+                painter = painterResource(id = com.planetoto.customer_component.R.drawable.placeholder_avatar)
+            ) {
+
+            }
         }
     }
 }
