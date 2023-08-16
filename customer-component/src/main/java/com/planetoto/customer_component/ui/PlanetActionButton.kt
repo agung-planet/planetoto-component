326b214c2261cc6d9c4eef166bea6f836e39b20c
agung <agung.kurniawan@oddy.id>
2023-08-16 15:44:07 +0700

    [] use variable font

--- customer-component/src/main/java/com/planetoto/customer_component/ui/PlanetActionButton.kt
+++ customer-component/src/main/java/com/planetoto/customer_component/ui/PlanetActionButton.kt
@@ -10,7 +10,12 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
@@ -68,7 +73,7 @@ fun PlanetActionButton(
     ) {
         Text(
             text = text.capitalizeWords(),
-            fontFamily = FontFamily(Font(R.font.figtree)),
+            fontFamily = FontFamily(Font(R.font.figtree_variable)),
             fontSize = 11.sp,
             fontWeight = FontWeight.W600,
             lineHeight = 13.2.sp,
